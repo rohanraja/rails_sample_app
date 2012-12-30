17fe26d004bad2a5597d5cb4b9e6067dbfee1d29
rohan1020 <rohanraja9@gmail.com>
2012-12-31 01:45:30 +0530

    tests not working, have_selector?

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,15 +3,32 @@ require 'spec_helper'
 describe PagesController do
 
   describe "GET 'home'" do
+    
     it "returns http success" do
       get 'home'
       response.should be_success
     end
+    
+    it "returns the right title" do
+    
+      get 'home'
+      response.should have_selector("title", :content => "RoR Sample")
+
+    end
+    
+    
   end
 
   describe "GET 'contact'" do
     it "returns http success" do
       get 'contact'
+      response.should be_success
+    end
+  end
+  
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
       response.should be_success
     end
   end
