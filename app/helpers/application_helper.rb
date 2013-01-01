3ed91fbeecb2e94d1b5e5d03084b016f390cbee3
rohan1020 <rohanraja9@gmail.com>
2013-01-01 08:41:36 +0530

    Helpers small working.. title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+	#Return a title on per page basis
+  
+  def title
+  	base_title = "RoR"
+  	
+  	if @title.nil?
+  		"#{base_title} - No"
+  	else
+  		"#{base_title} #{@title}"
+  		
+  	end
+  
+  
+  end
+  
+  
 end
